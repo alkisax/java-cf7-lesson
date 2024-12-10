af2b56921d6aade5911a3d22924d8b59f8ad306f
Alkis <alkisax@gmail.com>
2024-12-10 20:01:33 +0200

    initial commit

---
+++ src/gr/aueb/cf/ch1/HelloWorld.java
@@ -0,0 +1,22 @@
+package gr.aueb.cf.ch1;
+
+/**
+ * documantation comment
+ * τυπώνει  hello coding factory σε standard output
+ * @author alkis
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // line comment prints Hello...
+
+        /*
+        * block comments
+        * για πάνω απο μια γραμμή
+         */
+        System.out.println("Hello Coding Factory");
+
+
+    }
+}
+
