8936a18cdfc885b198e3ea643c9e0e5df0a6ff7c
Alkis <alkisax@gmail.com>
2024-12-21 18:10:07 +0200

    Remove Exercises submodule

---
+++ src/gr/aueb/cf/ch12/Scoping.java
@@ -0,0 +1,26 @@
+package gr.aueb.cf.ch12;
+
+public class Scoping {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int result = 0;
+
+        result = add(a, b);
+        System.out.println(result);
+    }
+
+    public static int add(int a, int b){
+        return a + b;
+    }
+
+    public static int div(int a, int b){
+        int result = 0;
+        if (b==0){
+            boolean isZero = true;
+            System.out.println("is zero: " + isZero);
+            return result;
+        }
+        return a / b;
+    }
+}
