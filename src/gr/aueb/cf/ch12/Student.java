45963d301f4aba5f020586c8f31bd8aa25a858e3
Alkis <alkisax@gmail.com>
2024-12-24 09:23:05 +0200

    minor changes

--- src/gr/aueb/cf/ch12/Student.java
+++ src/gr/aueb/cf/ch12/Student.java
@@ -1,7 +1,79 @@
 package gr.aueb.cf.ch12;
 
+/**
+ * java bean
+ * μια κλαση είναι bean όταν έχει -private πεδια -default constructor -geteres & setters με συμβαση + serializable
+ */
+
 public class Student {
-    public int id;
-    public String firstname;
-    public String lastname;
+    private int id;
+    private String firstname;
+    private String lastname;
+
+
+    // ftiaxtikan ola me alt+insert
+    // default constructor
+    public Student() {
+    }
+    //overloaded constructor
+    public Student (int id, String firstname, String lastname) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    //    //default constructor
+//    public Student (){
+//        id = 0;
+//        firstname = null;
+//        lastname = null;
+//    }
+//
+//    public int getId() {
+//        return this.id;
+//    }
+//
+//    public void setId (int id) {
+//        this.id = id;
+//    }
+//
+//    public String getFirstname() {
+//        return this.firstname;
+//    }
+//
+//    public void setFirstname(String firstname) {
+//        this.firstname = firstname;
+//    }
+//
+//    public String getLastname() {
+//        return this.lastname;
+//    }
+//
+//    public void setLastname(String lastname) {
+//        this.lastname = lastname;
+//    }
 }
--- src/gr/aueb/cf/ch12/StudentDemo.java
+++ src/gr/aueb/cf/ch12/StudentDemo.java
@@ -3,10 +3,24 @@ package gr.aueb.cf.ch12;
 public class StudentDemo {
     public static void main(String[] args) {
         Student student = new Student();
+        Student bob = new Student(2, "Bob", "D.");  // Invoke Overloaded Constructs and populate instance
 
-        student.id = 1;
-        student.firstname = "Alice";
-        student.lastname = "W.";
+        bob.setLastname("Dylan");
+
+        student.setId(1);
+        student.setFirstname("Alice");
+        student.setLastname("w.");
+
+//        System.out.println("id: " + student.id);
+//        System.out.println("first name: " + student.firstname);
+//        System.out.println("last name: " + student.lastname);
+
+        System.out.println("Id: " + student.getId());
+        System.out.println("Firstname: " + student.getFirstname());
+        System.out.println("Lastname: " + student.getLastname());
+
+
+        System.out.println("Student instances' count: " + Student.getStudentsCount());
         
     }
 }
