13ecdd5a3e075d1d27ad3d811295adfdf86e1548
Alkis <alkisax@gmail.com>
2024-12-24 08:36:41 +0200

    minor changes

--- src/gr/aueb/cf/ch12/StudentDemo.java
+++ src/gr/aueb/cf/ch12/StudentDemo.java
@@ -2,7 +2,11 @@ package gr.aueb.cf.ch12;
 
 public class StudentDemo {
     public static void main(String[] args) {
-        Student alice = new Student();
+        Student student = new Student();
+
+        student.id = 1;
+        student.firstname = "Alice";
+        student.lastname = "W.";
         
     }
 }
