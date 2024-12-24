6ea77108df558379951c217b2874b1c76f70a90b
Alkis <alkisax@gmail.com>
2024-12-24 14:09:25 +0200

    minor changes

--- src/gr/aueb/cf/ch12/Main.java
+++ src/gr/aueb/cf/ch12/Main.java
@@ -1,13 +1,24 @@
 package gr.aueb.cf.ch12;
 
-import gr.aueb.cf.ch12.model.Teacher;
-import gr.aueb.cf.ch12.model.User;
+import gr.aueb.cf.ch12.model.*;
+
+import javax.sound.midi.Soundbank;
+import java.sql.SQLOutput;
 
 public class Main {
     public static void main(String[] args) {
 
         Teacher teacher = new Teacher(1, "Athanasios", "Androutsos");
         User user = new User(1, "Kostas", "Mask", "cmask", "12345", true);
+        Customer customer = new Customer(1, "Andrew", "dimitrioy", "876543289", "6947733075", "Athens", "Attica", "Athens", "Patission", "76", "10434");
+        Point point = new Point(0, 10);
+        Order order = new Order(1, 5.5, "Meat", "ordered");
+
+        System.out.printf("point {%d, %d}\n", point.getX(), point.getY());
+        System.out.println("teacher full name: " + teacher.getFirstname() + ", " + teacher.getLastname());
+        System.out.println("User username: " + user.getUsername());
+        System.out.println("Customer  Vat: " + customer.getVatRegistretionNo());
+        System.out.println("Order Timestamp" + order.getFormatedTimestamp());
 
 
     }
