1f19df49d79171ca5ae56be8c2fc541e8622f92c
Alkis <alkisax@gmail.com>
2025-02-05 14:10:20 +0200

    minor

--- src/gr/aueb/cf/ch19/doubly_linked_list/DoublyLinkedList.java
+++ src/gr/aueb/cf/ch19/doubly_linked_list/DoublyLinkedList.java
@@ -38,4 +38,77 @@ public class DoublyLinkedList<T> {
 
         tail = tmp;
     }
+
+
+    /**
+     * Time-complexity O(1)
+     * @return  the removed node.
+     */
+
+    //Μετακοινουμε το head στο head next και το παίρνει ο garbage collector.
+    //αν δεν υπάρχει κανένα δεν αλλαζει κατι. αν υπάρχει ένα πρέπει και ο tail να δειξει στο ιδιο
+    public Node<T> removeFirst() {
+        Node<T> nodeToReturn;
+
+        //φτιαξαμε μια βοηθητικη isEmpty
+        if (isEmpty()) return null;
+
+        nodeToReturn = head;
+        //δηλ ελέγχω αν είναι ένα μόνο
+        if (head.getNext() == null) tail = null;
+        head = head.getNext();
+        head.setPrev(null);
+        return nodeToReturn;
+    }
+
+    /**
+     * Time-complexity O(1)
+     * @return  the removed node.
+     */
+    // το tail πρεπει να πάει στο προηγούμενο. και ο προτελευταιός να βαλει ως next null(δηλαδη να αποκοπεί το τελευταιο)
+    public Node<T> removeLast() {
+        Node<T> nodeToReturn;
+        if (isEmpty() || head.getNext() == null) {
+            return removeFirst();
+        }
+
+        nodeToReturn = tail;
+        tail = tail.getPrev();
+        tail.setNext(null);
+        return nodeToReturn;
+    }
+
+    public Node<T> get(T t) {
+        Node<T> nodeToReturn = null;
+        // an n=null τοτε φτάσαμε στο τελευταίο οταν n!=null είμαστε προτελευταίο και μετά n.getNext και φτάνει τελευταίο
+        for (Node<T> n = head; n != null; n = n.getNext()) {
+            if (n.getItem().equals(t)) {
+                nodeToReturn = n;
+                break;
+            }
+        }
+        return nodeToReturn;
+    }
+
+    /**
+     * Time-complexity O(n)
+     */
+    public void traverse() {
+        for (Node<T> n = head; n != null; n = n.getNext()) {
+            System.out.println(n.getItem());
+        }
+    }
+
+    /**
+     * Time-complexity O(n)
+     */
+    public void traverseReverse() {
+        for (Node<T> n = tail; n != null; n = n.getPrev()) {
+            System.out.println(n.getItem());
+        }
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
 }
