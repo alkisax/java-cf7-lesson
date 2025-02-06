e0db1ef51f1d08c1a1e843117c3808a9506b1b11
Alkis <alkisax@gmail.com>
2025-02-06 15:45:31 +0200

    maps streams

---
+++ src/gr/aueb/cf/ch19/streams/FilterMain.java
@@ -0,0 +1,35 @@
+package gr.aueb.cf.ch19.streams;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class FilterMain {
+    public static void main(String[] args) {
+        List<String> cities = List.of("Athens", "London", "Toronto", "Tunis", "Athens");
+
+        // to var είναι για να αποοφύγω να γράφω και στις δυο μεριές list<String>
+        //List<String> tCities = cities.stream()
+        var tCities = cities.stream()
+                .filter(city -> city.startsWith("T"))
+                .collect(Collectors.toList());          // Java8+, mutable List
+
+        var tCities2 = cities.stream()
+                .filter(city -> city.length() >= 6)
+                .toList();                              // Java16+, immutable list
+
+        tCities.forEach(System.out::println);
+
+        Set<String> uniqueCityNames = cities.stream().collect(Collectors.toSet());
+        // Set<String> uniqueCityNames = new HashSet<>(cities);
+        uniqueCityNames.forEach(System.out::println);
+
+
+        String strCities = cities.stream()
+                .collect(Collectors.joining(", "));
+
+        // String strCities = String.join(", ", cities);
+        System.out.println(strCities);
+
+    }
+}
