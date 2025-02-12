8362966848f74be0734dec479fa30c0ccaf21879
Alkis <alkisax@gmail.com>
2025-02-12 15:06:34 +0200

    regex enum lambda

---
+++ src/gr/aueb/cf/ch20/lambdas/Teacher.java
@@ -0,0 +1,65 @@
+package gr.aueb.cf.ch20.reflection;
+
+public class Teacher {
+    private Long id;
+    private String firstname;
+    private String lastname;
+
+    public Teacher() {
+
+    }
+
+    public Teacher(Long id, String firstname, String lastname) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public void sayHello() {
+        System.out.println("Hello");
+    }
+
+    public void saySomething(String message) {
+        System.out.println(message);
+    }
+
+    //gpt
+    public void printInstanceTeacher() {
+        System.out.println(this);
+    }
+
+    public static void printStaticTeacher(Teacher teacher) {
+        System.out.println(teacher);
+    }
+
+    @Override
+    public String toString() {
+        return "Teacher{id=" + id + ", firstname='" + firstname + "', lastname='" + lastname + "'}";
+    }
+
+
+}
